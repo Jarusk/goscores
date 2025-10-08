93a3e3da7dc2c2f00ea20a9765143037e050fc7c
Matt Yaraskavitch <yaraskavitch.matt@gmail.com>
2025-10-07 21:21:25 -0400

    WIP

--- internal/providers/providers.go
+++ internal/providers/providers.go
@@ -4,8 +4,40 @@ import (
 	"github.com/jarusk/goscores/internal/models"
 )
 
-type Sport string
-type League string
+type Sport int
+type League int
+
+const (
+	Baseball Sport = iota
+	Hockey
+)
+
+func (s Sport) String() string {
+	switch s {
+	case Hockey:
+		return "hockey"
+	case Baseball:
+		return "baseball"
+	default:
+		return "unknown"
+	}
+}
+
+const (
+	MLB League = iota
+	NHL
+)
+
+func (l League) String() string {
+	switch l {
+	case MLB:
+		return "mlb"
+	case NHL:
+		return "nhl"
+	default:
+		return "unknown"
+	}
+}
 
 type Provider interface {
 	GetName() string
